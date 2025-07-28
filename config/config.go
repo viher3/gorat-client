e0cc62f8b080f1407a4f0e6e96d88a493fc9c0c6
Alberto Vian <alberto.vian@digimobil.es>
2025-07-28 18:04:35 +0200

    Create config file

---
+++ config/config.go
@@ -0,0 +1,8 @@
+package config
+
+const (
+	CcServerAddress             = "192.168.1.0:1234"
+	DefaultTimeoutInSeconds     = 30
+	WaitTimeUntilRetryInSeconds = 60 * 60 * 5 // 5 minutes
+	Version                     = "0.0.1"
+)
