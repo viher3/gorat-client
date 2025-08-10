1e75b54daa1fa578805838802155241b09de161d
Alberto Vian <alberto.vian@digimobil.es>
2025-08-11 00:29:45 +0200

    Create serverConfig.go file

--- config/config.go
+++ config/config.go
@@ -1,17 +1,21 @@
 package config
 
 type Config struct {
-	CcServerAddress             string
-	DefaultTimeoutInSeconds     int
+	ServerAddress               string
+	ServerConnectionMode        string
+	TimeoutInSeconds            int
 	WaitTimeUntilRetryInSeconds int
 	Version                     string
 }
 
+const DefaultAppVersion = "0.0.1"
+
 func NewConfig() *Config {
 	return &Config{
-		CcServerAddress:             "192.168.1.0:1234",
-		DefaultTimeoutInSeconds:     30,
-		WaitTimeUntilRetryInSeconds: 60 * 60 * 5,
-		Version:                     "0.0.1",
+		ServerAddress:               "192.168.1.0:1234",
+		ServerConnectionMode:        ServerConnectionModeWebsocket,
+		TimeoutInSeconds:            DefaultTimeoutInSeconds,
+		WaitTimeUntilRetryInSeconds: 60 * 60 * DefaulWaitTimeUntilRetryInMinutes,
+		Version:                     DefaultAppVersion,
 	}
 }
---
+++ config/serverConfig.go
@@ -0,0 +1,5 @@
+package config
+
+const ServerConnectionModeWebsocket = "websocket"
+const DefaultTimeoutInSeconds = 30
+const DefaulWaitTimeUntilRetryInMinutes = 5
